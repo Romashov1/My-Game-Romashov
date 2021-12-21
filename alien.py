bc77fd5987e8ee4b0dc0c4d4406e8e0347d5cfac
[Akifev] <[vladakifev@gmail.com]>
2021-12-21 21:31:18 +0300

    прописываем пришельца

--- alien.py
+++ alien.py
@@ -10,7 +10,7 @@ class Alien(Sprite):
         self.screen = screen
         self.ai_settings = ai_settings
 
-        self.image = pygame.image.load('images/alien.png')
+        self.image = pygame.image.load('images/alien.jpg')
         self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
